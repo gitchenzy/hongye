a4528b0c8184fee3a2608d7cd8af4b32e53eafcc
gitchenzy <625244441@qq.com>
2017-09-18 11:46:07 +0800

    bind and likes

--- Application/Home/Controller/CenterController.class.php
+++ Application/Home/Controller/CenterController.class.php
@@ -86,7 +86,6 @@ class CenterController extends Controller {
         $this -> display();
     }
     public function loadNotes(){
-
         $type = I('type')?I('type'):2;
         $where['pay_type'] = $type;
         $where['user_id'] = $this -> user_id;
@@ -99,7 +98,64 @@ class CenterController extends Controller {
         }else{
             $this -> error('没有更多数据！');
         }
+    }
+    //绑定手机
+    public function bind(){
 
+        $this -> display();
+    }
+    //发送短信
+    public function send_sms(){
+        $phone = I('phone');
+        $type = I('type');
+        $string = get_string();
+        $code= $string->rand_string(4,1);
+        $res = send_sms($code,$phone);
+        if($res){
+            $info['code'] = $type.$code.$phone;
+            $info['code_time'] = time();
+            session('sms',$info);
+            $this -> success('发送成功！');
+        }else{
+            $this -> error('验证码发送失败');
+        }
+    }
+    //发送绑定手机
+    public function sub_bind(){
+        $phone = I('phone');
+        $type = I('type');
+        $code = I('code');
+        $info = session('sms');
+        if($type.$code.$phone != $info['code']){
+            $this -> error('验证码错误！');
+        }
+        if(time() > $info['code_time']*5*60){
+            $this -> error('验证码过期，请重新发送！');
+        }
+        //验证完了就开始修改手机号码 //线查询手机是否绑定
+        $result = M('users') -> where(['phone'=>$phone,'del'=>0]) -> find();
+        if($result){
+            $this -> error('该手机已经绑定过！');
+        }
+        $where['id'] = $this -> user_id;
+        $res = M('users') -> where($where) -> save(['phone'=>$phone]);
+        if($res){
+            $this -> success('手机绑定成功！');
+        }else{
+            $this -> error('手机绑定失败！');
+        }
+    }
+    //我的关注
+    public function likes(){
+        $where['user_id'] = $this -> user_id;
+        $res = M('user_attention') -> where($where) -> order('time desc') -> limit(0,5) ->select();
+        foreach($res as &$v){
+            $v['pic'] = M('project') -> where(['id'=>$v['project_id']]) -> getfield('pic');
+            $v['time'] = date('Y-m-d',$v['time']);
+        }
+        $this -> assign('title','我的关注');
+        $this -> assign('info',$res);
+        $this -> display();
     }
 
 }
