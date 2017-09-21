acea2c25fa7dcd8178fc7444937fe3f3c14955e0
DricHao <473164786@qq.com>
2017-09-21 20:18:15 +0800

    j

--- WebRoot/Public/Home/js/o_2.js
+++ WebRoot/Public/Home/js/o_2.js
@@ -5,6 +5,11 @@ $(function(){
 	$('.first_lowboxin:eq(0)').click(function(){
 		$('.first_box').css('display','none');
 	})
+	$('.first_box textarea').focus(function(){
+		if($('.first_box textarea').html()==='我也说一句'){
+			$('.first_box textarea').html('');
+		}
+	})
 	/*$('first_lowboxin:eq(1)').click(function(){
 		$ajax()
 	})*/
