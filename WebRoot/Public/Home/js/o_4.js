cc00854bc62a8ea34bb795338ae073ed4f50a425
DricHao <473164786@qq.com>
2017-09-21 14:17:56 +0800

    j

--- WebRoot/Public/Home/js/o_4.js
+++ WebRoot/Public/Home/js/o_4.js
@@ -1,35 +1,9 @@
 $(function(){
-var w_1=$(".helftopbox:eq(1)");
-var w_2=$(".helftopbox:eq(1)>h1 a");
-var w_3=$(".helftopbox:eq(0)");
-var w_4=$(".helftopbox:eq(0)>h1 a");
-var w_5=$(".pay_pic:eq(0)>img");
-var w_6=$(".pay_pic:eq(1)>img");
-w_1.click(function(){
-    w_1.css("border-bottom","2px solid #D33D3E");
-    w_3.css("border","none");
-    w_4.css("color","#CCCCCC");
-    w_2.css("color","#D33D3E");
-})
-w_3.click(function(){
-    w_1.css("border-bottom","none");
-    w_3.css("border-bottom","2px solid #D33D3E");
-    w_4.css("color","#D33D3E");
-    w_2.css("color","#CCCCCC"); 
-})
-w_5.click(function(){
-    if(w_5.attr("src")=="__PUBLIC__/Home/images/评论.png"){
-        w_5.attr("src","__PUBLIC__/Home/images/评论1.png");
-    }else{
-        w_5.attr("src","__PUBLIC__/Home/images/评论.png");
-    }
-})
-w_6.click(function(){
-    if(w_6.attr("src")=="__PUBLIC__/Home/images/关注2.png"){
-        w_6.attr("src","__PUBLIC__/Home/images/关注1.png");
-    }else{
-        w_6.attr("src","__PUBLIC__/Home/images/关注2.png");
-    }
-})
-
+    $('.pay_pic:eq(1)').click(function(){
+       if($('.pay_pic:eq(1)').attr('src')==='__PUBLIC__/Home/images/new/star_1.png'){
+           $('.pay_pic:eq(1)').attr('src','__PUBLIC__/Home/images/new/stars.png');
+       }else {
+           $('.pay_pic:eq(1)').attr('src','__PUBLIC__/Home/images/new/star_1.png')
+       }
+    })
 })
--- WebRoot/Public/Home/js/setting.js
+++ WebRoot/Public/Home/js/setting.js
@@ -1,8 +1,183 @@
 $(function(){
-    var cark=$(window).width();
-    var lark=0.8*cark;
-    var bark=$(".first_inbox");
-    var jark=$(".first_inbox>textarea");
-    bark.css("width",lark);
-    jark.css("width",lark)
+    var cropper;
+    $(".imgset_i").on("change", function () {
+        var fr = new FileReader();
+        var file = this.files[0];
+
+        if (!/image\/\w+/.test(file.type)) {
+            alert(file.name + "不是图片文件！");
+            return false;
+        } else if (file.size > 2 * 1024 * 1024) {
+            alert('图片大小不能超过2M');
+            return false;
+        }
+
+        fr.readAsDataURL(file);
+        fr.onload = function () {
+            //这里初始化cropper
+            console.log(fr);
+            $('.image').attr('src',fr.result);
+            iniCropper()
+        };
+        $('.imgset_a').html('重新选择');
+        $('.imgset_a').click(function ij(){
+            if( $('.imgset_a').html()==='重新选择'){
+                location=location;
+            }
+        })
+    });
+
+    var croppable = false;
+    function iniCropper() {
+        var $image = $('.image'),
+            image = $image[0];
+        cropper = new Cropper(image, {
+            dragMode: 'move',
+            aspectRatio: 1,
+            autoCropArea: 0.5,
+            restore: false,
+            viewMode: 1,
+            guides: false,
+            center: false,
+            highlight: false,
+            cropBoxMovable: false,
+            cropBoxResizable: false,
+            toggleDragModeOnDblclick: false,
+            zoomable:true,
+            ready: function () {
+                croppable = true;
+            }
+        });
+    }
+
+    $('.imgset_b').on('click', function () {
+        var croppedCanvas;
+        var rectCanvas;
+        var rectImage;
+
+        if (!croppable) {
+            return false;
+        }
+        // Crop
+        croppedCanvas = cropper.getCroppedCanvas();
+        //Rect
+        rectCanvas = getRectCanvas(croppedCanvas);
+        // Show
+        rectImage = document.createElement('img');
+        rectImage.src = rectCanvas.toDataURL();
+
+        $('.result').html('').append(rectImage);
+
+        //var form=document.forms[0];
+        var formData = new FormData();   //这里连带form里的其他参数也一起提交了,如果不需要提交其他参数可以直接FormData无参数的构造函数
+
+        //convertBase64UrlToBlob函数是将base64编码转换为Blob
+        formData.append("filename", convertBase64UrlToBlob(rectCanvas.toDataURL()));  //append函数的第一个参数是后台获取数据的参数名,和html标签的input的name属性功能相同
+        //ajax 提交form
+
+        return false;//不提交
+        $.ajax({
+            url: 'url',
+            type: "POST",
+            data: formData,
+            dataType: "text",
+            processData: false,         // 告诉jQuery不要去处理发送的数据
+            contentType: false,        // 告诉jQuery不要去设置Content-Type请求头
+
+            success: function (res) {
+                var data = JSON.parse(res);
+                if (data.status) {
+                    hideLoading();
+                    showTips(data.msg);
+                    setTimeout(function () {
+                        location.href = 'url?t=' + (new Date()).getTime();
+                    }, 200);
+                } else {
+                    console.log(data);
+                }
+            },
+            xhr: function () {            //在jquery函数中直接使用ajax的XMLHttpRequest对象
+                var xhr = new XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function (evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+                        console.log("正在提交..." + percentComplete.toString() + '%');        //在控制台打印上传进度
+                    }
+                }, false);
+
+                return xhr;
+            }
+
+        });
+    });
+
+//绘制矩形canvas
+    function getRectCanvas(sourceCanvas) {
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        var width = sourceCanvas.width;
+        var height = sourceCanvas.height;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        context.imageSmoothingEnabled = true;
+        context.drawImage(sourceCanvas, 0, 0, width, height);
+        context.globalCompositeOperation = 'destination-in';
+        context.beginPath();
+        context.rect(0, 0, width, height);
+        context.fill();
+
+        return canvas;
+    }
+
+    /**
+     * 将以base64的图片url数据转换为Blob
+     * @param urlData
+     * 用url方式表示的base64图片数据
+     */
+    function convertBase64UrlToBlob(urlData) {
+        var bytes = window.atob(urlData.split(',')[1]);       //去掉url的头，并转换为byte
+        //处理异常,将ascii码小于0的转换为大于0
+        var ab = new ArrayBuffer(bytes.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < bytes.length; i++) {
+            ia[i] = bytes.charCodeAt(i);
+        }
+        return new Blob([ab], { type: 'image/png' });
+    }//绘制矩形canvas
+    function getRectCanvas(sourceCanvas) {
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        var width = sourceCanvas.width;
+        var height = sourceCanvas.height;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        context.imageSmoothingEnabled = true;
+        context.drawImage(sourceCanvas, 0, 0, width, height);
+        context.globalCompositeOperation = 'destination-in';
+        context.beginPath();
+        context.rect(0, 0, width, height);
+        context.fill();
+
+        return canvas;
+    }
+
+    /**
+     * 将以base64的图片url数据转换为Blob
+     * @param urlData
+     * 用url方式表示的base64图片数据
+     */
+    function convertBase64UrlToBlob(urlData) {
+        var bytes = window.atob(urlData.split(',')[1]);       //去掉url的头，并转换为byte
+        //处理异常,将ascii码小于0的转换为大于0
+        var ab = new ArrayBuffer(bytes.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < bytes.length; i++) {
+            ia[i] = bytes.charCodeAt(i);
+        }
+        return new Blob([ab], { type: 'image/jpeg' });
+    }
 })
