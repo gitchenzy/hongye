d86d73ae182b67d127a592905e3fe4cac8f4ea51
gitchenzy <625244441@qq.com>
2017-09-28 21:10:47 +0800

    xiaoxi

--- Application/Home/Controller/MassageController.class.php
+++ Application/Home/Controller/MassageController.class.php
@@ -8,9 +8,17 @@ class MassageController extends Controller {
     }
     public function massage(){
         session('index',4);
-
+        $user = get_user_info();
+        if(!$user){
+            session('back_url','Massage/massage');
+            $this -> assign('error','请先登录！');
+        }
+        $type = I('type')?I('type'):1;
+        $where['type'] = $type;
+        $where['user_id'] = $user['user_id'];
+        $info = M('infos') -> where($where) -> select();
+        $this -> assign('info',$info);
+        $this -> assign('type',$type);
         $this -> display();
     }
-
-
 }
