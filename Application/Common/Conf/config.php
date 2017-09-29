625e3c926c21b44762ffce516f836d3afcafd223
gitchenzy <625244441@qq.com>
2017-09-30 00:34:03 +0800

    edit

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -9,12 +9,12 @@ return array(
     /* 数据库配置 */
     'DB_DEPLOY_TYPE' => 1,
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'hongye', // 数据库名
     'DB_USER'   => 'root', // 用户名
 
  //   'DB_PWD'    => 'root',  // 密码
-    'DB_PWD'    => '2017hybbms.com',  // 密码
+    'DB_PWD'    => '2017HYbbms.com',  // 密码
 
     'DB_PORT'   => '3306', // 端口
     //'DB_PREFIX' => '', // 数据库表前缀
