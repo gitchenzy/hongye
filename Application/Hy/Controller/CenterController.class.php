1ae0e835caf884c60c9342769636c67fc78fcc75
gitchenzy <625244441@qq.com>
2017-11-03 11:46:48 +0800

    jindu

---
+++ Application/Hy/Controller/CenterController.class.php
@@ -0,0 +1,14 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+namespace Hy\Controller;
+class CenterController extends CommonController {
+    protected function _initialize(){
+        parent::_initialize();
+    }
+    public function index(){
+
+
+    }
+
+
+}
